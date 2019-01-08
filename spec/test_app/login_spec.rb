84433321350222efd72e343112e591bc7874bc5d
quantranits <quantranits@gmail.com>
2019-01-08 14:42:24 +0700

    edit file

--- spec/test_app/login_spec.rb
+++ spec/test_app/login_spec.rb
@@ -11,10 +11,4 @@ describe "Verify User can login successfully" do
     on(LoginPage).submit_login_info
     expect(on(MyNotesPage).loaded?).to be true
   end
-
-  it 'Should create new note successfully' do
-    on(MyNotesPage).add_note_element.click
-    note_data = on(AddNewNotePage).create_new_note
-    expect(on(MyNotesPage)).to have_note_info note_data
-  end
 end
